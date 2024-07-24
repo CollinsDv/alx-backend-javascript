9b556350d055889f1bcf5cec736620b535a6e541
Collins Kimani <mwas9446.ck@gmail.com>
2024-07-25 00:22:32 +0300

    add file 3 and utils.js
    Entails importing external functions from utils and formating promises returned

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils';
+
+// collectively resolve all promises and
+// log body firstName lastName to the console.
+function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then((values) => {
+      const photo = values[0].body;
+      const { firstName } = values[1];
+      const { lastName } = values[1];
+
+      console.log(`${photo} ${firstName} ${lastName}`);
+    });
+}
+
+export default handleProfileSignup;
