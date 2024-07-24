96f73e64d38a3ce22bdc5ce0a84adf0b4a034e55
Collins Kimani <mwas9446.ck@gmail.com>
2024-07-25 01:20:43 +0300

    add file 5
    This uses a function that automatically rejects operations by default with error object

---
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -0,0 +1,7 @@
+// function that return a Promise rejecting with an Error and the string $fileName cannot be processed
+
+export default function uploadPhoto(filename) {
+  return new Promise((reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  });
+}
