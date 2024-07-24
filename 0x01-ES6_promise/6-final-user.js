a94290fb5eeaa3911c0ee6f22c0c4547b80afc76
Collins Kimani <mwas9446.ck@gmail.com>
2024-07-25 02:11:09 +0300

    add file 6
    ensures all promises settle and results are formatted accordingly

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+// write and export func handleProfileSignup
+// accepts firstName, lastName and fileName
+// it uses imported funcs signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js
+// on all promise settlement, it returns:
+// [
+//   {
+//     status: status_of_the_promise,
+//     value: value or error returned by the Promise
+//   },
+//   ...
+// ]
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
+}
