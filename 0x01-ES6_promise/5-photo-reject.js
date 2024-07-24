0e92aee420c7b34574cd7d43c94e5322a5ce3837
Collins Kimani <mwas9446.ck@gmail.com>
2024-07-25 01:41:29 +0300

    refactor: Simplify uploadPhoto function in 5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,7 +1,6 @@
-// function that return a Promise rejecting with an Error and the string $fileName cannot be processed
+// function that return a Promise rejecting with an Error
+// and the string $fileName cannot be processed
 
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
