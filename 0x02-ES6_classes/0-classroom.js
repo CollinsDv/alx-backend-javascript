5908b0c0c387905eab458d87e2a3cd2fb3c357d3
Collins Kimani <mwas9446.ck@gmail.com>
2024-07-25 16:32:48 +0300

    add README and file 0
    file 0 entails a class definition that accepts one arguement

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+// implement class ClassRoom
+// prototype: export default class ClassRoom
+// accepts one attribute maxStudentSize(number), assigned to _maxStudentSize
+
+export default class ClassRoom {
+  constructor(maxStudentSize) {
+    this._maxStudentSize = maxStudentSize;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,3 @@
+# `ES6 classes`
+Leraning all about classes in javascript and related concepts like inheritance and polymorphism
+
